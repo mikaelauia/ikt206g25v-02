ba0b6958354414146a8109561eb281216138d0cf
Mikael Andersstuen <mikaela@uia.no>
2025-02-03 13:32:39 +0100

    Added assignment 2

---
+++ Testlib.Tests/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using Testlib;
+using Xunit;
+
+namespace Testlib.Tests
+{
+    public class TestStringLibrary
+    {
+        [Theory]
+        [InlineData("Alphabet", true)]
+        [InlineData("alphabet", false)]
+        [InlineData("1234", false)]
+        [InlineData(" ", false)]
+        [InlineData("", false)]
+        [InlineData(null, false)]
+        public void TestStartsWithUpper(string input, bool expected)
+        {
+            bool result = input.StartsWithUpper();
+            Assert.Equal(expected, result);
+        }
+    }
+}
